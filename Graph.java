c61c4fe38fa6ba6c6224c026cbf83d9b049084ca
Frank Tutino <gyrotoo@gmail.com>
2021-11-29 17:09:06 -0500

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,25 +1,25 @@
-// A real Adjacency Matrix representation in Java
+// Adjacency Matrix representation in Java
 
 public class Graph {
-    private boolean adjMatrix[][];
+    private int adjMatrix[][];
     private int numVertices;
 
     // Initialize the matrix
     public Graph(int numVertices) {
         this.numVertices = numVertices;
-        adjMatrix = new boolean[numVertices][numVertices];
+        adjMatrix = new int[numVertices][numVertices];
     }
 
     // Add edges
-    public void addEdge(int i, int j) {
-        adjMatrix[i][j] = true;
-        adjMatrix[j][i] = true;
+    public void addEdge(int i, int j,int weight) {
+        adjMatrix[i][j] = weight;
+        //adjMatrix[j][i] = j;
     }
 
     // Remove edges
     public void removeEdge(int i, int j) {
-        adjMatrix[i][j] = false;
-        adjMatrix[j][i] = false;
+        adjMatrix[i][j] = 0;
+        adjMatrix[j][i] = 0;
     }
 
     // Print the matrix
@@ -27,8 +27,8 @@ public class Graph {
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < numVertices; i++) {
             s.append(i + ": ");
-            for (boolean j : adjMatrix[i]) {
-                s.append((j ? 1 : 0) + " ");
+            for (int j : adjMatrix[i]) {
+                s.append((j  + " "));
             }
             s.append("\n");
         }
@@ -38,11 +38,11 @@ public class Graph {
     public static void main(String args[]) {
         Graph g = new Graph(4);
 
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
+        g.addEdge(0, 1,2);
+        g.addEdge(0, 2,3);
+        g.addEdge(1, 2,1);
+        g.addEdge(2, 0,4);
+        g.addEdge(2, 3,2);
 
         System.out.print(g.toString());
     }
